548ce67dc2126d3c324d8eb50a64bfce1086143e
rlodhiya <rlodhiya2502@gmail.com>
2025-01-01 20:04:49 +0000

    Add geolocation feature to OpenStreetMap component with toast notifications

--- projects/directory/frontend/DriveWiseApp/src/app/components/openstreetmap/openstreetmap.component.ts
+++ projects/directory/frontend/DriveWiseApp/src/app/components/openstreetmap/openstreetmap.component.ts
@@ -1,31 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-openstreetmap',
   templateUrl: './openstreetmap.component.html',
   styleUrls: ['./openstreetmap.component.scss'],
 })
-export class OpenstreetmapComponent  implements OnInit {
+export class OpenstreetmapComponent implements OnInit {
 
-  constructor() { }
+  private map: L.Map;
+
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {
     this.initMap();
   }
 
   private initMap(): void {
-    const map = L.map('map', {
+    this.map = L.map('map', {
       center: [51.505, -0.09],
       zoom: 13,
     });
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors',
-    }).addTo(map);
+    }).addTo(this.map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    L.marker([51.5, -0.09]).addTo(this.map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.');
   }
 
+  public async locateCurrentPosition(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        this.map.setView([lat, lon], 13);
+        L.marker([lat, lon]).addTo(this.map)
+          .bindPopup('You are here!')
+          .openPopup();
+      });
+    } else {
+      this.presentToast('Geolocation is not supported by this browser.');
+    }
+  }
+
+  private async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
 }
