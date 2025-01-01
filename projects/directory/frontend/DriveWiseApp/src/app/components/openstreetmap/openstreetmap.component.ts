ed845a73d0c72f3a85743c4731a87f2f6ae6fcc0
rlodhiya <rlodhiya2502@gmail.com>
2025-01-01 20:17:41 +0000

    Refactor OpenStreetMap component to improve structure and remove unused map initialization

--- projects/directory/frontend/DriveWiseApp/src/app/components/openstreetmap/openstreetmap.component.ts
+++ projects/directory/frontend/DriveWiseApp/src/app/components/openstreetmap/openstreetmap.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
-import { ToastController } from '@ionic/angular';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { ToastController, IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-openstreetmap',
@@ -9,37 +9,20 @@ import { ToastController } from '@ionic/angular';
 })
 export class OpenstreetmapComponent implements OnInit {
 
-  private map: L.Map;
+ 
 
   constructor(private toastController: ToastController) { }
 
   ngOnInit() {
-    this.initMap();
+    
   }
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [51.505, -0.09],
-      zoom: 13,
-    });
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors',
-    }).addTo(this.map);
-
-    L.marker([51.5, -0.09]).addTo(this.map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.');
-  }
-
-  public async locateCurrentPosition(): void {
+  public async locateCurrentPosition() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-        this.map.setView([lat, lon], 13);
-        L.marker([lat, lon]).addTo(this.map)
-          .bindPopup('You are here!')
-          .openPopup();
+        
       });
     } else {
       this.presentToast('Geolocation is not supported by this browser.');
